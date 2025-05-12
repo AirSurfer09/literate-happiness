ded64e0be700d52fea2c4b788262704ef43f0dc7
AirSurfer09 <saurav@convai.com>
2025-05-12 17:32:18 +0530

    referr policy

--- next.config.js
+++ next.config.js
@@ -5,19 +5,30 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // Apply to all routes
-        headers: [
-          {
-            key: "Referrer-Policy",
-            value: "no-referrer", // Preventing referrer exposure
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   distDir: 'dist',
+//   images: {
+//     unoptimized: true,
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)', // Apply to all routes
+//         headers: [
+//           {
+//             key: 'Referrer-Policy',
+//             value: 'no-referrer', // Preventing referrer exposure
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+//
+// module.exports = nextConfig;
