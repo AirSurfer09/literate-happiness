8b952c9063b4a19bc3db0014478a3e17bf4e8f3b
AirSurfer09 <saurav@convai.com>
2025-05-12 19:20:33 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,34 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-//   distDir: "dist",
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-//
-// module.exports = nextConfig;
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   distDir: "dist",
   images: {
     unoptimized: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // Apply to all routes
-        headers: [
-          {
-            key: "Referrer-Policy",
-            value: "no-referrer", // Preventing referrer exposure
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   distDir: 'dist',
+//   images: {
+//     unoptimized: true,
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)', // Apply to all routes
+//         headers: [
+//           {
+//             key: 'Referrer-Policy',
+//             value: 'no-referrer', // Preventing referrer exposure
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+//
+// module.exports = nextConfig;
